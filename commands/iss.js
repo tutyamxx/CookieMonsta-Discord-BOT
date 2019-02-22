3028d73411627497f322a98a6ddeaaea1c46874c
Alexandru M <tutygoo@gmail.com>
2019-02-22 16:15:14 +0000

    Indent Fix

--- commands/iss.js
+++ commands/iss.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) =>
 {
 	var user = message.author;
 
-    message.channel.startTyping();
+	message.channel.startTyping();
 
 	await getJSON("http://api.open-notify.org/iss-now.json", async function(error, response)
 	{
@@ -15,18 +15,18 @@ module.exports.run = async (bot, message, args) =>
 			return await message.channel.send(":no_entry: Sorry, something went wrong while fetching NASA ISS location... :no_entry:").then(()=> message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
 		}
 
-        var iLocLatitude = JSON.stringify(response.iss_position.latitude).replace(/"/g, '');
-        var iLocLongitude = JSON.stringify(response.iss_position.longitude).replace(/"/g, '');
+		var iLocLatitude = JSON.stringify(response.iss_position.latitude).replace(/"/g, '');
+		var iLocLongitude = JSON.stringify(response.iss_position.longitude).replace(/"/g, '');
 
-        const embed = new Discord.RichEmbed()
-        .setAuthor("Cookie Monsta | International Space Station Location", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
-        .setColor("#00BFFF")
-        .setDescription(":satellite_orbital: **Location:** :satellite_orbital:\n\n:satellite: **Latitude:** " + iLocLatitude + "°\n:satellite: **Longitude:** " + iLocLongitude + "°")
-        .setThumbnail("https://i.imgur.com/T3zXl0F.png")
-        .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
-        .setTimestamp();
-
-        await message.channel.send({embed}).then(()=> message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+		const embed = new Discord.RichEmbed()
+		.setAuthor("Cookie Monsta | International Space Station Location", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
+		.setColor("#00BFFF")
+		.setDescription(":satellite_orbital: **Location:** :satellite_orbital:\n\n:satellite: **Latitude:** " + iLocLatitude + "°\n:satellite: **Longitude:** " + iLocLongitude + "°")
+		.setThumbnail("https://i.imgur.com/T3zXl0F.png")
+		.setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
+		.setTimestamp();
+		
+		await message.channel.send({embed}).then(()=> message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
     });
 };
 
