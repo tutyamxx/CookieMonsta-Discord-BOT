056fb3dd294237e6866db8123fa1357226d073f7
Alexandru M <tutygoo@gmail.com>
2019-02-22 15:44:15 +0000

    Indent fix

--- commands/apod.js
+++ commands/apod.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) =>
 {
 	var user = message.author;
 
-    message.channel.startTyping();
+	message.channel.startTyping();
 
 	let NasaMessageEdit = await message.channel.send("Pinging **NASA** database :satellite: for the feed...");
 
@@ -24,20 +24,20 @@ module.exports.run = async (bot, message, args) =>
 			return await NasaMessageEdit.edit(":no_entry: Sorry, there is **no picture** :milky_way: available for today! :no_entry:").then(()=> message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
 		}
 
-        var szCopyRight = JSON.stringify(response.copyright).replace(/"/g, '').replace(/\\n/g, ' ');
-        var szTitle = JSON.stringify(response.title).replace(/"/g, '');
+		var szCopyRight = JSON.stringify(response.copyright).replace(/"/g, '').replace(/\\n/g, ' ');
+		var szTitle = JSON.stringify(response.title).replace(/"/g, '');
 
-        var szImageHDURL = JSON.stringify(response.hdurl).replace(/"/g, '');
+		var szImageHDURL = JSON.stringify(response.hdurl).replace(/"/g, '');
 
-        const embed = new Discord.RichEmbed()
-        .setAuthor("Cookie Monsta | Astronomy Picture of the Day", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
-        .setColor("#8A2BE2")
-        .setDescription(":milky_way: **Picture Name:** `" + szTitle + "`\n\n:bust_in_silhouette: **Copyright:copyright: :** " + szCopyRight + "\n\n\n\n:link: [Download FullHD Image](" + szImageHDURL + ") :link:")
-        .setImage(szImageHDURL)
-        .setThumbnail((bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
-        .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
+		const embed = new Discord.RichEmbed()
+		.setAuthor("Cookie Monsta | Astronomy Picture of the Day", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
+		.setColor("#8A2BE2")
+		.setDescription(":milky_way: **Picture Name:** `" + szTitle + "`\n\n:bust_in_silhouette: **Copyright:copyright: :** " + szCopyRight + "\n\n\n\n:link: [Download FullHD Image](" + szImageHDURL + ") :link:")
+		.setImage(szImageHDURL)
+		.setThumbnail((bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
+		.setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
 
-        return await NasaMessageEdit.edit({embed}).then(()=> message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+		return await NasaMessageEdit.edit({embed}).then(()=> message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
     });
 };
 
