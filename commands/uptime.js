cb6c5eb50b3f265a5b6564d3f46914be8f854869
Alexandru M <tutygoo@gmail.com>
2019-02-22 16:25:43 +0000

    Indent Fix

--- commands/uptime.js
+++ commands/uptime.js
@@ -7,8 +7,8 @@ const CustomFunctions = require("../functions/funcs.js");
 module.exports.run = async (bot, message, args) =>
 {
 	const embed = new Discord.RichEmbed()
-    .setAuthor("Cookie Monsta | BOT Uptime", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
-    .setColor(26367)
+	.setAuthor("Cookie Monsta | BOT Uptime", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
+	.setColor(26367)
 	.setDescription(`:robot: **Uptime:** ${CustomFunctions.secondsToString(process.uptime())}\n\n:desktop: **Uptime:** ${CustomFunctions.secondsToString(os.uptime())}`)
 	.setThumbnail("https://i.imgur.com/df79Q6S.png")
 	.setFooter("⏰")
