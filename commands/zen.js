5d14734045bea32524740c7b6fac2ce29722859b
Alex Maxim <alex.maxim@3gtelecoms.net>
2019-10-19 00:58:58 +0100

    :zap:

--- commands/zen.js
+++ commands/zen.js
@@ -1,16 +1,30 @@
+const Discord = require("discord.js");
 const Needle = require("needle");
 
 module.exports.run = async (bot, message, args) =>
 {
+    const user = message.author;
+
     message.channel.startTyping();
 
     await Needle.get("https://api.github.com/zen", async function(error, response)
     {
-        if(!error && response.statusCode == 200)
+        if(!error && response.statusCode === 200)
         {
             let ZenWords = await response.body.replace(/"/g, '').replace(/'/g, '').replace(/\[/g, '').replace(/\]/g, '').replace(/\\/g, '"');
 
-            await message.channel.send("``Cookie Monsta | GitHub Zen`` :tanabata_tree:\n" + "```diff\n- " + ZenWords + "```").then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+            const DiscordRichEmbed = new Discord.RichEmbed()
+            .setAuthor("Cookie Monsta | GitHub Zen", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
+            .setColor(16777037)
+            .setThumbnail("https://i.imgur.com/6pFPKvA.png")
+            .setDescription(":tanabata_tree: " + ZenWords)
+            .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
+
+            await message.channel.send({ embed: DiscordRichEmbed }).then(function (message)
+            {
+                message.react(":tanabata_tree:");
+
+            }).then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
         }
 
         else
