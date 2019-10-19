864162438391fe7f557836cb8783a1d5feb78b49
Alex Maxim <alex.maxim@3gtelecoms.net>
2019-10-19 19:53:56 +0100

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const getJSON = require("get-json");
+
+module.exports.run = async (bot, message, args) =>
+{
+    const user = message.author;
+
+    message.channel.startTyping();
+
+    await getJSON("https://some-random-api.ml/meme", async function (error, response)
+    {
+        if(error)
+        {
+            return await message.channel.send(":no_entry: Aww snap, something went wrong lebrowsky! Try again? :no_entry:").then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+        }
+
+        let szRandomMemeImage = JSON.stringify(await response.image).replace(/"/g, '');
+        let szRandomMemeCaption = JSON.stringify(await response.caption).replace(/"/g, '');
+
+        const DiscordRichEmbed = new Discord.RichEmbed()
+        .setAuthor("Cookie Monsta | Random Meme", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
+        .setColor("#" + (Math.random() * 0xFFFFFF << 0).toString(16))
+        .setImage(szRandomMemeImage)
+        .setThumbnail("https://i.imgur.com/PAHa5SU.png")
+        .setDescription(szRandomMemeCaption)
+        .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
+
+        await message.channel.send({ embed: DiscordRichEmbed }).then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+    });
+};
+
+module.exports.help =
+{
+    name: "meme"
+};
