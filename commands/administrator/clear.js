6dde484d8d3a8a3c39dfd63b7723dfa760664b02
Alex Maxim <alex.maxim@3gtelecoms.net>
2019-10-20 15:54:51 +0100

    Update clear.js

--- commands/administrator/clear.js
+++ commands/administrator/clear.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 const RandomColorsPurge =
 [
     9699539,
@@ -17,44 +19,22 @@ module.exports.run = async (bot, message, args) =>
         return await message.channel.send(":no_entry: You can't mate! Fucking biblical... :laughing: :no_entry:");
     }
 
-    await message.delete();
-
-    const user = message.author;
-
-    const fetched = await message.channel.fetchMessages({limit: 99});
-    await message.channel.bulkDelete(fetched).catch(error => message.reply(`:no_entry_sign: ${error} :no_entry_sign:`));
-
-    await message.channel.send(
+    await message.delete().then(async () =>
     {
-        embed:
-        {
-            color: RandomColorsPurge[Math.floor(Math.random() * RandomColorsPurge.length)],
+        const user = message.author;
 
-            description: ":earth_africa: :x: :regional_indicator_c::regional_indicator_h::regional_indicator_a::regional_indicator_t:     :regional_indicator_p::regional_indicator_u::regional_indicator_r::regional_indicator_g::regional_indicator_e::regional_indicator_d: :x: :earth_africa:\n\n",
+        const fetched = await message.channel.fetchMessages({ limit: 99 });
+        await message.channel.bulkDelete(fetched).catch(error => message.reply(`:no_entry_sign: ${error} :no_entry_sign:`));
 
-            fields:
-            [
-                {
-                    name: "Chat has been PURGED due to high number of messages",
-                    value: "NOOOOO! My cookies! 👀",
-                }
-            ],
+        const DiscordRichEmbed = new Discord.RichEmbed()
+        .setColor(RandomColorsPurge[Math.floor(Math.random() * RandomColorsPurge.length)])
+        .setDescription(":earth_africa: :x: :regional_indicator_c::regional_indicator_h::regional_indicator_a::regional_indicator_t:     :regional_indicator_p::regional_indicator_u::regional_indicator_r::regional_indicator_g::regional_indicator_e::regional_indicator_d: :x: :earth_africa:\n\n")
+        .addField("Chat has been PURGED due to high number of messages", "NOOOOO! My cookies! 👀")
+        .setImage("https://i.imgur.com/UCuPAjF.jpg")
+        .setFooter("Char Purged by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
+        .setTimestamp()
 
-            image:
-            {
-                url: "https://i.imgur.com/UCuPAjF.jpg",
-                width: 800,
-                height: 800
-            },
-
-            timestamp: new Date(),
-
-            footer:
-            {
-                icon_url: (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL,
-                text: "Chat Purged by @" + user.username
-            }
-        }
+        await message.channel.send({ embed: DiscordRichEmbed });
     });
 };
 
