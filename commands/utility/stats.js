270b5bc2c4a8f71a037247913fd20203adb3471c
Alex Maxim <alex.maxim@3gtelecoms.net>
2019-10-20 01:15:50 +0100

    Update stats.js

--- commands/utility/stats.js
+++ commands/utility/stats.js
@@ -3,7 +3,7 @@ const os = require("os");
 const CustomFunctions = require("../../functions/funcs.js");
 const CookieMonsta = require("../../CookieMonstaBOT.js");
 
-const szBotVersionNow = "7.0.0";
+const szBotVersionNow = "7.0.1";
 
 module.exports.run = async (bot, message, args) =>
 {
@@ -11,8 +11,8 @@ module.exports.run = async (bot, message, args) =>
     .setAuthor("Cookie Monsta | BOT Stats", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL :  bot.user.avatarURL)
     .setColor("#FFE4B5")
     .addField(":bar_chart: Client Stats", "**" + bot.guilds.size + "** servers\n**" + bot.channels.size + "** channels\n**" + bot.users.size + "** users\n**" + CookieMonsta.iCommandNumber + "** commands", true )
-    .addField(":chart_with_upwards_trend: Server Stats", "Running on: **" + os.platform() + "**\nArchitecture: **" + os.arch() + "**\nVs: **" + os.release() + "**\nMemory: **" + CustomFunctions.bytesToSize(process.memoryUsage().heapUsed) + "**/**" + CustomFunctions.bytesToSize(os.totalmem()) + "**", true)
-    .addField(":chart: Various Stats", "Version: **v" + szBotVersionNow + "**\nAuthor: **tuty4amxx#3969**\nNode Version: **" + process.version + "**", true)
+    .addField(":chart_with_upwards_trend: Server Stats", "Running on: **" + os.platform() + "**\nArchitecture: **" + os.arch() + "**\nVs: **" + os.release() + "**\nRAM: **" + CustomFunctions.bytesToSize(process.memoryUsage().heapUsed) + "**/**" + CustomFunctions.bytesToSize(os.totalmem()) + "**", true)
+    .addField(":chart: Various Stats", "Version: **v" + szBotVersionNow + "**\nAuthor: **tuty4amxx#3969**\nNode.JS: **" + process.version + "**", true)
 
     await message.channel.send({ embed: DiscordRichEmbed });
 };
