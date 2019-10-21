2495a2668df6fbacb806fd138afd0065a498312c
Alex Maxim <alex.maxim@3gtelecoms.net>
2019-10-21 23:17:21 +0100

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,7 +1,15 @@
+const szRandomFetchingMessages =
+[
+    "Fething ping", "Calculating ping", "Testing ping",
+    "Warping to other dimension", ":eyes:", "Exploiting API",
+    "Getting some nerdy stuff", "If I was only good at math",
+    "Yup, incoming", "Executing metasploit command"
+];
+
 module.exports.run = async (bot, message, args) =>
 {
-    const m = await message.channel.send("Ping?");
-    await m.edit(`Pong! :ping_pong: Latency is **${m.createdTimestamp - message.createdTimestamp}ms**. API Latency is **${Math.round(bot.ping)}ms**`);
+    const szMessagePing = await message.channel.send("<:cookiemonsta:634866060465537034> **|** " + szRandomFetchingMessages[Math.floor(Math.random() * szRandomFetchingMessages.length)] + " :satellite: ...");
+    await szMessagePing.edit(`<:cookiemonsta:634866060465537034> **|** :ping_pong: Latency is **${szMessagePing.createdTimestamp - message.createdTimestamp}ms**. API Latency is **${Math.round(bot.ping)}ms**`);
 };
 
 module.exports.help =
