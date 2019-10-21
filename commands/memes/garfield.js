8df679141d2d7ea7b438bfcae0c7123fac1784d4
Alex Maxim <alex.maxim@3gtelecoms.net>
2019-10-21 22:47:50 +0100

    Added a new command

---
+++ commands/memes/garfield.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const Jimp = require("jimp");
+const mergeImg = require("merge-img");
+
+module.exports.run = async (bot, message, args) =>
+{
+    message.channel.startTyping();
+
+    const GarFieldComics = [];
+    let GarfieldImage = [];
+
+    GarFieldComics[0] = Math.floor(Math.random() * 52);
+    GarFieldComics[1] = Math.floor(Math.random() * 54);
+    GarFieldComics[2] = Math.floor(Math.random() * 52);
+
+    GarfieldImage[0] = "http://www.gmiller.net/misc/Garfield/MIXED/A_G_" + GarFieldComics[0] + ".png";
+    GarfieldImage[1] = "http://www.gmiller.net/misc/Garfield/MIXED/B_G_" + GarFieldComics[1] + ".png";
+    GarfieldImage[2] = "http://www.gmiller.net/misc/Garfield/MIXED/C_G_" + GarFieldComics[2] + ".png";
+
+    await mergeImg([GarfieldImage[0], GarfieldImage[1], GarfieldImage[2]], { color: 0xffffffff, align: "center", offset: 7, margin: { top: 7, right: 7, bottom: 7, left: 7 } }).then(async (image) =>
+    {
+        image.getBuffer(Jimp.MIME_PNG, async (err, buffer) =>
+        {
+            if(err)
+            {
+                return console.log("\x1b[31m*\x1b[0m Whoops! There is your error: \x1b[31m" + err + "\x1b[0m");
+            }
+
+            await message.channel.send(new Discord.Attachment(buffer, "garfield_comics.png")).then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+        });
+    });
+};
+
+module.exports.help =
+{
+    name: "garfield"
+};
