b81e6554dec7af81f7eab1deb8b9803862b6c9d1
Alex Maxim <alex.maxim@3gtelecoms.net>
2019-10-24 01:19:50 +0100

    Update help.js

--- commands/help/help.js
+++ commands/help/help.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) =>
 {
     message.channel.send("Info sent. Please check your DM :mailbox_with_mail:");
 
-    let FormatHelpMessage = ":cookie: <:cookiemonsta:634866060465537034> Hi there pleb! I'm ``Cookie Monsta`` <:cookiemonsta:634866060465537034> :cookie: \n\n\nTo see all the available commands, click :point_right: [HERE!](https://github.com/tutyamxx/CookieMonsta-BOT/blob/master/README.md#commands) :point_left:";
+    let FormatHelpMessage = ":cookie: <:cookiemonsta:634866060465537034> Hi there pleb! I'm ``Cookie Monsta`` <:cookiemonsta:634866060465537034> :cookie: \n\n\nTo see all the available commands, click :point_right: [HERE!](https://tutyamxx.github.io/cookie-monsta-website/commands.html) :point_left:";
 
     const DiscordRichEmbed = new Discord.RichEmbed()
     .setTitle("Cookie Monsta | Information and Commands")
