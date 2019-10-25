eccba4409a181e1818d7e4273417b4a9de98d531
Alex Maxim <alex.maxim@3gtelecoms.net>
2019-10-25 10:24:07 +0100

    2 new commands

---
+++ commands/fun/quote.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+const getJSON = require("get-json");
+const CustomFunctions = require("../../functions/funcs.js");
+
+module.exports.run = async (bot, message, args) =>
+{
+    const user = message.author;
+
+    message.channel.startTyping();
+
+    await getJSON("https://quote-garden.herokuapp.com/quotes/random", async function (error, response)
+    {
+        if(error)
+        {
+            return await message.channel.send(":no_entry: Sorry, I can't seem to find a random quote at the moment...  :crying_cat_face:  :no_entry:").then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+        }
+
+        // --| Remove "" from start and end of string
+        let RandomQuoteText = await response.quoteText.trim();
+        let RandomQuoteAuthor = await response.quoteAuthor;
+
+        const DiscordRichEmbed = new Discord.RichEmbed()
+        .setAuthor("Cookie Monsta | Random Quote", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
+        .setColor("#cc33ff")
+        .setDescription(":speech_left:  *" + RandomQuoteText + "*\n\n\n**-** :label: " + (CustomFunctions.isEmpty(RandomQuoteAuthor) ? "Unknown Author" : RandomQuoteAuthor))
+        .setThumbnail("https://i.imgur.com/BdjxI8e.png")
+        .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
+
+        await message.channel.send({ embed: DiscordRichEmbed }).then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+    });
+};
+
+module.exports.help =
+{
+    name: "quote"
+};
---
+++ commands/utility/nameavatar.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const CustomFunctions = require("../../functions/funcs.js");
+const Jimp = require("jimp");
+
+module.exports.run = async (bot, message, szArgs) =>
+{
+    if(CustomFunctions.isEmpty(szArgs[0]))
+    {
+        return await message.reply(" :no_entry: this parameter can't be empty you scrub :facepalm: ! Type **!nameavatar** ``<your text>`` :no_entry:");
+    }
+
+    if(szArgs[0].length > 30)
+    {
+        return await message.reply(" :no_entry: too many characters in this name! Maximum **30** :no_entry:");
+    }
+
+    message.channel.startTyping();
+
+    let ArgumentText = szArgs.join(" ");
+    let RandomAvatar = Math.round(Math.random());
+
+    let GenerateRandomAvatar = "https://api.adorable.io/avatar" + (RandomAvatar === 1 ? "" : "s") +  "/285/" + encodeURI(ArgumentText) + ".png";
+    let GetAvatarBuffer = await Jimp.read(GenerateRandomAvatar);
+
+    await GetAvatarBuffer.getBuffer(Jimp.MIME_PNG, async (err, buffer) =>
+    {
+        if(err)
+        {
+            return console.log("\x1b[31m*\x1b[0m Whoops! There is your error: \x1b[31m" + err + "\x1b[0m").then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+        }
+
+        await message.channel.send(new Discord.Attachment(buffer, "nameavatar.png")).then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+    });
+};
+
+module.exports.help =
+{
+    name: "nameavatar"
+};
