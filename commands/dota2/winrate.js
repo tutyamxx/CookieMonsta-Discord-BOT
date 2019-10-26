0d1e0f23f19a4cc49cd0a09c2b777b848abc1298
Alex Maxim <tutygoo@gmail.com>
2019-10-26 15:43:27 +0100

    Update winrate.js

--- commands/dota2/winrate.js
+++ commands/dota2/winrate.js
@@ -75,7 +75,7 @@ module.exports.run = async (bot, message, szArgs) =>
     }).catch(async (error) =>
     {
         return await message.channel.send(":no_entry: Sorry, I couldn't find this Steam profile: ``" + szArgs[0] + "``  :disappointed_relieved:  :no_entry:").then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
-    });;
+    });
 };
 
 module.exports.help =
