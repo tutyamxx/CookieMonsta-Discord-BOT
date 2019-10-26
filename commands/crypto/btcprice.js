600efa337e0cbf6d61d3e4acd602c63dcb8c2b87
Alex Maxim <tutygoo@gmail.com>
2019-10-26 19:24:37 +0100

    Update btcprice.js

--- commands/crypto/btcprice.js
+++ commands/crypto/btcprice.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const getJSON = require("get-json");
-const CustomFunctions = require("../../functions/funcs.js");
 
 module.exports.run = async (bot, message, args) =>
 {
