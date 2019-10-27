728ba61d040035819807d65b892d30ef6b985a57
Alex Maxim <tutygoo@gmail.com>
2019-10-27 14:34:42 +0000

    2 new commands

---
+++ commands/animals/koala.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const getJSON = require("get-json");
+
+module.exports.run = async (bot, message, args) =>
+{
+    const user = message.author;
+
+    await getJSON("https://some-random-api.ml/img/koala", async function (error, response)
+    {
+        if (error)
+        {
+            return await message.channel.send(":no_entry: Sorry, no koalas to generate. Try again later :koala:  :no_entry:").then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+        }
+
+        let RandomKoala = JSON.stringify(await response.link).replace(/"/g, '');
+
+        const DiscordRichEmbed = new Discord.RichEmbed()
+        .setAuthor("Cookie Monsta | Random Koala", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
+        .setColor("#dddad0")
+        .setImage(RandomKoala)
+        .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
+
+        await message.channel.send({ embed: DiscordRichEmbed }).then(async (message) =>
+        {
+            await message.react(":koala:");
+        });
+    });
+};
+
+module.exports.help =
+{
+    name: "koala"
+};
---
+++ commands/animals/koalafact.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const getJSON = require("get-json");
+
+module.exports.run = async (bot, message, args) =>
+{
+    const user = message.author;
+
+    message.channel.startTyping();
+
+    await getJSON("https://some-random-api.ml/facts/koala", async function (error, response)
+    {
+        if (error)
+        {
+            return await message.channel.send(":no_entry: Did you know? Koalas are so protected that, they might have disappeared? :cry:  :no_entry:").then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+        }
+
+        // --| Remove "" from start and end of string
+        let KoalaFactToString = JSON.stringify(await response.fact).replace(/"/g, '').replace(/\\/g, "``");
+
+        const DiscordRichEmbed = new Discord.RichEmbed()
+        .setAuthor("Cookie Monsta | Dog Facts", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
+        .setColor("#dddad0")
+        .setDescription(KoalaFactToString)
+        .setThumbnail("https://i.imgur.com/Ch7vTxz.png")
+        .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
+
+        await message.channel.send({ embed: DiscordRichEmbed }).then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+    });
+};
+
+module.exports.help =
+{
+    name: "koalafact"
+};
