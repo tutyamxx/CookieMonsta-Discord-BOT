b98a36241ba34efff6aaff2a8bee77a269d39904
tutyamxx <tutygoo@gmail.com>
2019-11-07 17:33:13 +0000

    Update setbanner.js

--- commands/utility/setbanner.js
+++ commands/utility/setbanner.js
@@ -50,7 +50,7 @@ module.exports.run = async (bot, message, szArgs) =>
         {
             await DatabaseImport.CookieMonsta_SetUserCookies(GetGuildID, user.id, iUserCookies - 25000);
             await DatabaseImport.CookieMonsta_SetUserProfileBanner(GetGuildID, user.id, iUserBannerName.toString().padStart(2, "0") + ".png");
-            await message.channel.send("<:cookiemonsta:634866060465537034> **|** Okay, I have updated the profile banner :frame_photo: for " + user + " with: **" + iUserBannerName.toString().padStart(2, "0") + "**. Also stole **1500** :cookie: cookies from him L3L :wink:");
+            await message.channel.send("<:cookiemonsta:634866060465537034> **|** Okay, I have updated the profile banner :frame_photo: for " + user + " with: **" + iUserBannerName.toString().padStart(2, "0") + "**. Also stole **25000** :cookie: cookies from him L3L :wink:");
 
             break;
         }
