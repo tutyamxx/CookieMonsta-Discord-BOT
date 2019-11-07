af8d6aa88ac5afc3b7ec408d0ec55dce908d2ea3
tutyamxx <tutygoo@gmail.com>
2019-11-07 12:44:59 +0000

    Update setbanner.js

--- commands/utility/setbanner.js
+++ commands/utility/setbanner.js
@@ -7,6 +7,18 @@ module.exports.run = async (bot, message, szArgs) =>
     const user = message.author;
     const GetGuildID = message.guild.id;
 
+    if(!await DatabaseImport.CookieMonsta_UserExists(GetGuildID, user.id))
+    {
+        await DatabaseImport.CookieMonsta_CreateUser(GetGuildID, user.id, 150, 0, 1, "01.png");
+    }
+
+    const iUserCookies = await DatabaseImport.CookieMonsta_GetUserCookies(GetGuildID, user.id);
+
+    if(iUserCookies < 1500)
+    {
+        return await message.reply(":no_entry: you don't have enough cookies :cookie: to do that! :no_entry:");
+    }
+
     if(CustomFunctions.isEmpty(szArgs[0]))
     {
         return await message.reply(" :no_entry: this parameter can't be empty you scrub :facepalm: ! Type **!setbanner** ``<one banner name from 1 to 91>`` :no_entry:");
@@ -25,6 +37,7 @@ module.exports.run = async (bot, message, szArgs) =>
     {
         if(parseInt(BannersFromDatabase[i].png_file.slice(0, -4)) === parseInt(iUserBannerName))
         {
+            await DatabaseImport.CookieMonsta_SetUserCookies(GetGuildID, user.id, iUserCookies - 1500);
             await DatabaseImport.CookieMonsta_SetUserProfileBanner(GetGuildID, user.id, iUserBannerName.toString().padStart(2, "0") + ".png");
             await message.channel.send("<:cookiemonsta:634866060465537034> **|** Okay, I have updated the profile banner :frame_photo: for " + user + " with: **" + iUserBannerName.toString().padStart(2, "0") + "**");
 
