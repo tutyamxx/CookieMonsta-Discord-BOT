0dccf53ed11a2236a792f2849ab113648eee4b19
tutyamxx <tutygoo@gmail.com>
2019-11-13 02:38:26 +0000

    Update setprefix.js

--- commands/administrator/setprefix.js
+++ commands/administrator/setprefix.js
@@ -10,18 +10,25 @@ module.exports.run = async (bot, message, szArgs) =>
     {
         return await message.channel.send(":no_entry: You can't mate! Fucking biblical... :laughing: :no_entry:");
     }
-    
+
     if(CustomFunctions.isEmpty(szArgs[0]))
     {
         return await message.reply(" :no_entry: this parameter can't be empty you scrub :facepalm: ! You need to specify a prefix for Christ sake!  :no_entry:");
     }
 
     const ArgumentPrefix = szArgs[0].trim().toString();
+    const CheckEmojis = new RegExp("(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])", "gi");
+
+    if(CheckEmojis.test(ArgumentPrefix))
+    {
+        return await message.reply(" :no_entry: nice try :smirk:, but you can't use emojis for prefix! Try again!  :no_entry:");
+    }
+   
     const GetServerPrefix = await DatabaseImport.CookieMonsta_GetGuildPrefix(GetGuildID);
 
     if(ArgumentPrefix === GetServerPrefix.trim())
     {
-        return await message.reply(" :no_entry: Not happening! Your new prefix is the same as the current one ``" + GetServerPrefix.trim() + "`` for **" + ServerName+  "**!  :no_entry:");
+        return await message.reply(" :no_entry: Not happening! Your new prefix is the same as the current one ``" + GetServerPrefix.trim() + "`` for **" + ServerName + "**!  :no_entry:");
     }
 
     await DatabaseImport.CookieMonsta_SetGuildPrefix(GetGuildID, ArgumentPrefix);
