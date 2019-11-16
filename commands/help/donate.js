cfb8ce266fada680bb0ffeb7c26918006e49b920
tutyamxx <tutygoo@gmail.com>
2019-11-16 10:28:33 +0000

    Update donate.js

--- commands/help/donate.js
+++ commands/help/donate.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) =>
 
     const DiscordRichEmbed = new Discord.RichEmbed()
     .setAuthor("Cookie Monsta | Donate!", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
-    .setDescription(":ballot_box_with_check: Bot hosting is provided by me, using my own money and server.\n\n:ballot_box_with_check: That being said, if you enjoy this super duper awesome bot and want to `help` in keeping it alive, I would appreciate a small **1£** donation that will help me to pay the hosting and keep me focused in future updates!\n\n:ballot_box_with_check: I will update it anyway, plebs <:FeelsDabMan:634890825230319640>\n\n\n\:credit_card: You can donate via **PayPal** by clicking this [LINK](" + BotConfig.DiscordBOT_PayPal_Link.trim().toString() + ")\n\n\n\n:red_circle: ***YOU ARE NOT FORCED BY ANY MEANS TO DONATE!*** :red_circle:")
+    .setDescription(":ballot_box_with_check: Bot hosting is provided by me, using my own money and server.\n\n:ballot_box_with_check: That being said, if you enjoy this super duper awesome bot and want to `help` in keeping it alive, I would appreciate a small **1£** donation that will help me to pay the hosting and keep me focused in future updates!\n\n:ballot_box_with_check: I will update it anyway, plebs <:FeelsDabMan:634890825230319640>\n\n\n\:credit_card: You can buy me a coffee :coffee: by clicking this [LINK](" + BotConfig.DiscordBOT_PayPal_Link.trim().toString() + ")\n\n\n\n:red_circle: ***YOU ARE NOT FORCED BY ANY MEANS TO DONATE!*** :red_circle:")
     .setColor("#1E90FF")
     .setThumbnail("https://i.imgur.com/6aAZnYy.jpg")
     .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
