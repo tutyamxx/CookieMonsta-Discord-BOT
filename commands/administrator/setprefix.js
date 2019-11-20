8dd09f47f4fb5b8713eb509dff64fbfdaca71787
tutyamxx <tutygoo@gmail.com>
2019-11-20 13:33:45 +0000

    Update setprefix.js

--- commands/administrator/setprefix.js
+++ commands/administrator/setprefix.js
@@ -6,46 +6,48 @@ module.exports.run = async (bot, message, szArgs) =>
     const GetGuildID = message.guild.id;
     const ServerName = message.guild.name;
 
-    if(!message.channel.permissionsFor(message.member).has("ADMINISTRATOR", false)
-    || !message.channel.permissionsFor(message.member).has("BAN_MEMBERS", false))
+    if(message.channel.permissionsFor(message.member).has("ADMINISTRATOR", false) || message.channel.permissionsFor(message.member).has("MANAGE_GUILD", false))
+    {
+        if(CustomFunctions.isEmpty(szArgs[0]))
+        {
+            return await message.reply(" :no_entry: this parameter can't be empty you scrub :facepalm: ! You need to specify a prefix for Christ sake!  :no_entry:");
+        }
+
+        const ArgumentPrefix = szArgs[0].trim().toString();
+        const CheckEmojis = new RegExp("(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])", "gi");
+
+        if(CheckEmojis.test(ArgumentPrefix))
+        {
+            return await message.reply(" :no_entry: nice try :smirk:, but you can't use emojis for prefix! Try again!  :no_entry:");
+        }
+    
+        const CheckPrintableChars = new RegExp(/^[a-z0-9!"#$%&'()*+,./:;<=>?\[\]^_{|}~-]*$/, "i");
+
+        if(!CheckPrintableChars.test(ArgumentPrefix))
+        {
+            return await message.reply(" :no_entry: nice try :smirk:, but you can't use weird characters for prefix! Try again!  :no_entry:");
+        }
+
+        if(ArgumentPrefix.length > 3)
+        {
+            return await message.reply(" :no_entry: You cannot have more than **3** characters into your prefix! Try again!  :no_entry:");
+        }
+
+        const GetServerPrefix = await DatabaseImport.CookieMonsta_GetGuildPrefix(GetGuildID);
+
+        if(ArgumentPrefix === GetServerPrefix.trim())
+        {
+            return await message.reply(" :no_entry: Not happening! Your new prefix is the same as the current one ``" + GetServerPrefix.trim() + "`` for **" + ServerName + "**!  :no_entry:");
+        }
+
+        await DatabaseImport.CookieMonsta_SetGuildPrefix(GetGuildID, ArgumentPrefix);
+        await message.channel.send("<:cookiemonsta:634866060465537034> **|** Okay, I have updated the prefix for **" + ServerName + "** with ``" + ArgumentPrefix + "``");
+    }
+
+    else
     {
         return await message.channel.send(":no_entry: You can't mate! Fucking biblical... :laughing: :no_entry:");
     }
-
-    if(CustomFunctions.isEmpty(szArgs[0]))
-    {
-        return await message.reply(" :no_entry: this parameter can't be empty you scrub :facepalm: ! You need to specify a prefix for Christ sake!  :no_entry:");
-    }
-
-    const ArgumentPrefix = szArgs[0].trim().toString();
-    const CheckEmojis = new RegExp("(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])", "gi");
-
-    if(CheckEmojis.test(ArgumentPrefix))
-    {
-        return await message.reply(" :no_entry: nice try :smirk:, but you can't use emojis for prefix! Try again!  :no_entry:");
-    }
-   
-    const CheckPrintableChars = new RegExp(/^[a-z0-9!"#$%&'()*+,./:;<=>?\[\]^_{|}~-]*$/, "i");
-
-    if(!CheckPrintableChars.test(ArgumentPrefix))
-    {
-        return await message.reply(" :no_entry: nice try :smirk:, but you can't use weird characters for prefix! Try again!  :no_entry:");
-    }
-
-    if(ArgumentPrefix.length > 3)
-    {
-        return await message.reply(" :no_entry: You cannot have more than **3** characters into your prefix! Try again!  :no_entry:");
-    }
-
-    const GetServerPrefix = await DatabaseImport.CookieMonsta_GetGuildPrefix(GetGuildID);
-
-    if(ArgumentPrefix === GetServerPrefix.trim())
-    {
-        return await message.reply(" :no_entry: Not happening! Your new prefix is the same as the current one ``" + GetServerPrefix.trim() + "`` for **" + ServerName + "**!  :no_entry:");
-    }
-
-    await DatabaseImport.CookieMonsta_SetGuildPrefix(GetGuildID, ArgumentPrefix);
-    await message.channel.send("<:cookiemonsta:634866060465537034> **|** Okay, I have updated the prefix for **" + ServerName + "** with ``" + ArgumentPrefix + "``");
 };
 
 module.exports.help =
