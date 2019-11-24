48db239da11187c4fbd87d36536c0485bf685d0d
tutyamxx <tutygoo@gmail.com>
2019-11-24 23:56:47 +0000

    Updates

--- commands/games/shellgame.js
+++ commands/games/shellgame.js
@@ -11,54 +11,57 @@ module.exports.run = async (bot, message, args) =>
 {
     const user = message.author;
 
-    if(!message.member.guild.me.hasPermission("MANAGE_MESSAGES"))
+    if(message.channel.permissionsFor(message.member.guild.me).has("MANAGE_MESSAGES", false) && message.channel.permissionsFor(message.member.guild.me).has("ADD_REACTIONS", false))
     {
-        return await message.reply(":no_entry: I can't start a **Shell Game** session because I don't have enough permissions! :no_entry:")
-    }
-
-    if(bPlayingShell[user.id] === true)
-    {
-        return await message.reply(":no_entry: you are already in a **Shell Game** session! :no_entry:")
-    }
-
-    ShellGameID[user.id] = user.id;
-    bPlayingShell[user.id] = true;
-
-    UserShuffleShells[user.id] = Math.floor(( Math.random() * 3 ) + 1);
-
-    const DiscordRichEmbed = new Discord.RichEmbed()
-    .setAuthor("Cookie Monsta | Shell Game", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
-    .setColor(12582656)
-    .setDescription("**Shell Game**\n\nYou have to guess on which one of the shells is the :egg: hidden.\n\nChoose one of the shells to see if you were right, or click ❎ to end the game!\n\n\n:chestnut:	  :chestnut:     :chestnut:")
-    .setThumbnail("https://i.imgur.com/HeqXIRR.jpg")
-
-    if(!user.bot)
-    {
-        await message.channel.send({ embed: DiscordRichEmbed }).then(async msg =>
+        if(bPlayingShell[user.id] === true)
         {
-            await msg.react("\u0031\u20E3"); // 1
-            await msg.react("\u0032\u20E3"); // 2
-            await msg.react("\u0033\u20E3"); // 3
-            await msg.react("❎");			 // end
+            return await message.reply(":no_entry: you are already in a **Shell Game** session! :no_entry:")
+        }
 
-            iGameEndTimer[user.id] = setInterval (async function ()
+        ShellGameID[user.id] = user.id;
+        bPlayingShell[user.id] = true;
+
+        UserShuffleShells[user.id] = Math.floor(( Math.random() * 3 ) + 1);
+
+        const DiscordRichEmbed = new Discord.RichEmbed()
+        .setAuthor("Cookie Monsta | Shell Game", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
+        .setColor(12582656)
+        .setDescription("**Shell Game**\n\nYou have to guess on which one of the shells is the :egg: hidden.\n\nChoose one of the shells to see if you were right, or click ❎ to end the game!\n\n\n:chestnut:	  :chestnut:     :chestnut:")
+        .setThumbnail("https://i.imgur.com/HeqXIRR.jpg")
+
+        if(!user.bot)
+        {
+            await message.channel.send({ embed: DiscordRichEmbed }).then(async msg =>
             {
-                if(bPlayingShell[user.id] === false)
+                await msg.react("\u0031\u20E3"); // 1
+                await msg.react("\u0032\u20E3"); // 2
+                await msg.react("\u0033\u20E3"); // 3
+                await msg.react("❎");			 // end
+
+                iGameEndTimer[user.id] = setInterval (async function ()
                 {
-                    const DiscordRichEmbed1 = new Discord.RichEmbed()
-                    .setAuthor("Cookie Monsta | Shell Game", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
-                    .setColor(12582656)
-                    .setDescription(szShellGameDescription[user.id])
-                    .setThumbnail("https://i.imgur.com/HeqXIRR.jpg")
+                    if(bPlayingShell[user.id] === false)
+                    {
+                        const DiscordRichEmbed1 = new Discord.RichEmbed()
+                        .setAuthor("Cookie Monsta | Shell Game", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
+                        .setColor(12582656)
+                        .setDescription(szShellGameDescription[user.id])
+                        .setThumbnail("https://i.imgur.com/HeqXIRR.jpg")
 
-                    await msg.clearReactions();
-                    await msg.edit({ embed: DiscordRichEmbed1 });
+                        await msg.clearReactions();
+                        await msg.edit({ embed: DiscordRichEmbed1 });
 
-                    bot.clearInterval(iGameEndTimer[user.id]);
-                }
+                        bot.clearInterval(iGameEndTimer[user.id]);
+                    }
 
-            }, 1000);
-        });
+                }, 1000);
+            });
+        }
+    }
+
+    else
+    {
+        return await message.reply(":no_entry: I can't start a **Shell Game** session because I don't have enough permissions! :no_entry:");
     }
 };
 
--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -10,18 +10,18 @@ module.exports = async (bot, reaction, user) =>
             return;
         }
 
-        let GetGuildID = reaction.message.guild.id.toString();
+        let GetGuildID = await reaction.message.guild.id.toString();
         let RandomShellPrize = Math.floor(( Math.random() * 10 ) + 1);
 
         await DatabaseImport.CookieMonsta_CheckCreateUser(GetGuildID, user.id);
 
         const iUserCookies = await DatabaseImport.CookieMonsta_GetUserCookies(GetGuildID, user.id);
 
-        if(reaction.emoji.name === "\u0031\u20E3")
+        if(await reaction.emoji.name === "\u0031\u20E3")
         {
             if(1 === ShellGame.UserShuffleShells[user.id])
             {
-                reaction.remove(user);
+                await reaction.remove(user);
 
                 await DatabaseImport.CookieMonsta_SetUserCookies(GetGuildID, user.id, iUserCookies + RandomShellPrize);
                 ShellGame.szShellGameDescription[user.id] = "**Shell Game**\n\n\n\n:confetti_ball:  Congratulations " + user + ", you won **" + RandomShellPrize + "** cookies :cookie: this time!\n\n\n:egg:	  :chestnut:     :chestnut:";
@@ -29,16 +29,16 @@ module.exports = async (bot, reaction, user) =>
 
             else
             {
-                reaction.remove(user);
+                await reaction.remove(user);
                 ShellGame.szShellGameDescription[user.id] = "**Shell Game**\n\n\n\nSorry " + user + ", you lost! Good luck next time!\n\n\n:x:	  :chestnut:     :chestnut:";
             }
         }
 
-        else if(reaction.emoji.name === "\u0032\u20E3")
+        else if(await reaction.emoji.name === "\u0032\u20E3")
         {
             if(2 === ShellGame.UserShuffleShells[user.id])
             {
-                reaction.remove(user);
+                await reaction.remove(user);
 
                 await DatabaseImport.CookieMonsta_SetUserCookies(GetGuildID, user.id, iUserCookies + RandomShellPrize);
                 ShellGame.szShellGameDescription[user.id] = "**Shell Game**\n\n\n\n:confetti_ball:  Congratulations " + user + ", you won **" + RandomShellPrize + "** cookies :cookie: this time!\n\n\n:chestnut:	  :egg:     :chestnut:";
@@ -46,16 +46,16 @@ module.exports = async (bot, reaction, user) =>
 
             else
             {
-                reaction.remove(user);
+                await reaction.remove(user);
                 ShellGame.szShellGameDescription[user.id] = "**Shell Game**\n\n\n\nSorry " + user + ", you lost! Good luck next time!\n\n\n:chestnut:	  :x:     :chestnut:";
             }
         }
 
-        else if(reaction.emoji.name === "\u0033\u20E3")
+        else if(await reaction.emoji.name === "\u0033\u20E3")
         {
             if(3 === ShellGame.UserShuffleShells[user.id])
             {
-                reaction.remove(user);
+                await reaction.remove(user);
 
                 await DatabaseImport.CookieMonsta_SetUserCookies(GetGuildID, user.id, iUserCookies + RandomShellPrize);
                 ShellGame.szShellGameDescription[user.id] = "**Shell Game**\n\n\n\n:confetti_ball:  Congratulations " + user + ", you won **" + RandomShellPrize + "** cookies :cookie: this time!\n\n\n:chestnut:	  :chestnut:     :egg:";
@@ -63,14 +63,14 @@ module.exports = async (bot, reaction, user) =>
 
             else
             {
-                reaction.remove(user);
+                await reaction.remove(user);
                 ShellGame.szShellGameDescription[user.id] = "**Shell Game**\n\n\n\nSorry " + user + ", you lost! Good luck next time!\n\n\n:chestnut:	  :chestnut:     :x:";
             }
         }
 
-        else if(reaction.emoji.name === "❎")
+        else if(await reaction.emoji.name === "❎")
         {
-            reaction.remove(user);
+            await reaction.remove(user);
             ShellGame.szShellGameDescription[user.id] = "**Shell Game**\n\n\n\n\n:x:   ***GAME OVER***    :x:";
         }
 
