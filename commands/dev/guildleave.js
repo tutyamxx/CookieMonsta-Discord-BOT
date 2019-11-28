0621ebf217e0bdd3e024505ef5ed6e30002b168e
tutyamxx <tutygoo@gmail.com>
2019-11-28 11:34:44 +0000

    Update guildleave.js

--- commands/dev/guildleave.js
+++ commands/dev/guildleave.js
@@ -3,37 +3,45 @@ const CustomFunctions = require("../../functions/funcs.js");
 
 module.exports.run = async (bot, message, szArgs) =>
 {
-    if(message.author.id !== "266677298051153920")
+    if(bot)
     {
-        return await message.reply(" :no_entry_sign: you're not the Dev pleb :facepalm:  :no_entry_sign:");
+        if (message.author.id !== "266677298051153920")
+        {
+            return await message.reply(" :no_entry_sign: you're not the Dev pleb :facepalm:  :no_entry_sign:");
+        }
+
+        if (CustomFunctions.isEmpty(szArgs[0]))
+        {
+            return await message.reply(" :no_entry: argument can't be empty! :no_entry:");
+        }
+
+        const MentionedGuildParam = szArgs[0].trim().toString();
+        let GetGuild = await bot.guilds.get(MentionedGuildParam);
+
+        if (GetGuild === undefined)
+        {
+            return await message.reply(" Invalid guild or it seems that I have already left the: **" + MentionedGuildParam + "** guild :id: !");
+        }
+
+        await GetGuild.leave().catch(async (e) =>
+        {
+            return await message.reply(" I tried to leave this guild id :id:: **" + MentionedGuildParam + "** but I got this error: ``" + e.message + "`` !");
+        });
+
+        const DiscordRichEmbed = new Discord.RichEmbed()
+        .setAuthor("Cookie Monsta | Guild Leave Log", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
+        .setColor(2003199)
+        .setDescription("I have left the specified guild\n\n:id:: (**" + szArgs[0] + "**) !")
+        .setThumbnail((bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
+        .setTimestamp()
+
+        await message.author.send({ embed: DiscordRichEmbed });
     }
 
-    if(CustomFunctions.isEmpty(szArgs[0]))
+    else
     {
-        return await message.reply(" :no_entry: argument can't be empty! :no_entry:");
+        return await message.reply(" Sorry, something went wrong, you should probably check why :joy: !");
     }
-
-    const MentionedGuildParam = szArgs[0].trim().toString();
-    let GetGuild = await bot.guilds.get(MentionedGuildParam);
-
-    if(GetGuild === undefined)
-    {
-        return await message.reply(" Invalid guild or it seems that I have already left the: **" + MentionedGuildParam + "** guild :id: !");
-    }
-
-    await GetGuild.leave().catch(async (e) =>
-    {
-        return await message.reply(" I tried to leave this guild id :id:: **" + MentionedGuildParam + "** but I got this error: ``" + e.message + "`` !");
-    });
-
-    const DiscordRichEmbed = new Discord.RichEmbed()
-    .setAuthor("Cookie Monsta | Guild Leave Log", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
-    .setColor(2003199)
-    .setDescription("I have left the specified guild\n\n:id:: (**" + szArgs[0] + "**) !")
-    .setThumbnail((bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
-    .setTimestamp()
-
-    await message.author.send({ embed: DiscordRichEmbed });
 };
 
 module.exports.help =
