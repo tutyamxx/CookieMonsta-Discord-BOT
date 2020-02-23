5db9d56392f76e7ff5c1b6f11a085c1c50e0ebff
tutyamxx <tutygoo@gmail.com>
2020-02-23 21:40:43 +0000

    Update zen.js

--- commands/fun/zen.js
+++ commands/fun/zen.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const Needle = require("needle");
+const axios = require("axios");
 
 module.exports.run = async (bot, message, args) =>
 {
@@ -7,30 +7,26 @@ module.exports.run = async (bot, message, args) =>
 
     await message.channel.startTyping();
 
-    Needle.get("https://api.github.com/zen", async (error, response) =>
+    await axios.get("https://api.github.com/zen").then(async (response) =>
     {
-        if(!error && response.statusCode === 200)
+        const ZenWords = await response.data.replace(/"/g, "").replace(/'/g, "").replace(/\[/g, "").replace(/\]/g, "").replace(/\\/g, '"');
+
+        const DiscordRichEmbed = new Discord.RichEmbed()
+        .setAuthor("Cookie Monsta | GitHub Zen", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
+        .setColor(16777037)
+        .setThumbnail("https://i.imgur.com/6pFPKvA.png")
+        .setDescription(":tanabata_tree: " + ZenWords)
+        .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
+
+        await message.channel.send({ embed: DiscordRichEmbed }).then(async (message) =>
         {
-            let ZenWords = await response.body.replace(/"/g, '').replace(/'/g, '').replace(/\[/g, '').replace(/\]/g, '').replace(/\\/g, '"');
+            await message.react("🎋");
 
-            const DiscordRichEmbed = new Discord.RichEmbed()
-            .setAuthor("Cookie Monsta | GitHub Zen", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
-            .setColor(16777037)
-            .setThumbnail("https://i.imgur.com/6pFPKvA.png")
-            .setDescription(":tanabata_tree: " + ZenWords)
-            .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
-
-            await message.channel.send({ embed: DiscordRichEmbed }).then(async (message) =>
-            {
-                await message.react("🎋");
-
-            }).then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
-        }
-
-        else
-        {
-            return await message.channel.send(":no_entry: Something went wrong! Be patient, try again later! :sob:  :no_entry:").then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
-        }
+        }).then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
+        
+    }).catch(async () =>
+    {
+        return await message.channel.send(":no_entry: Something went wrong! Be patient, try again later! :sob:  :no_entry:").then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
     });
 };
 
