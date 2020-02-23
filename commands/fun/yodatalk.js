262a297566b47cefb7ec0368f4232fe3c73abb80
tutyamxx <tutygoo@gmail.com>
2020-02-23 21:31:34 +0000

    Update yodatalk.js

--- commands/fun/yodatalk.js
+++ commands/fun/yodatalk.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const getJSON = require("get-json");
+const axios = require("axios");
 const CustomFunctions = require("../../functions/funcs.js");
 
 module.exports.run = async (bot, message, szArgs) =>
@@ -15,15 +15,10 @@ module.exports.run = async (bot, message, szArgs) =>
 
     let ArgumentText = szArgs.join(" ");
 
-    await getJSON("http://yoda-api.appspot.com/api/v1/yodish?text=" + encodeURI(ArgumentText), async (error, response) =>
+    await axios.get("http://yoda-api.appspot.com/api/v1/yodish?text=" + encodeURI(ArgumentText)).then(async (response) =>
     {
-        if(error)
-        {
-            return await message.channel.send(":no_entry: Oh noes! master Yoda has dissapeared! Try again later. :sob:  :no_entry:").then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
-        }
-
         // --| Remove "" from start and end of string
-        let YodishString = JSON.stringify(await response.yodish).replace(/"/g, '');
+        const YodishString = JSON.stringify(await response.data.yodish).replace(/"/g, "");
 
         const DiscordRichEmbed = new Discord.RichEmbed()
         .setAuthor("Cookie Monsta | Yoda Talk", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
@@ -32,7 +27,11 @@ module.exports.run = async (bot, message, szArgs) =>
         .setThumbnail("https://i.imgur.com/9H5dC0O.jpg")
         .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
 
-        await message.channel.send({ embed: DiscordRichEmbed }).then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+        await message.channel.send({ embed: DiscordRichEmbed }).then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
+    
+    }).catch(async () =>
+    {
+        return await message.channel.send(":no_entry: Oh noes! master Yoda has dissapeared! Try again later. :sob:  :no_entry:").then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
     });
 };
 
