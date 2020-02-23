7f77c0d46a55457379689260bcdad76359d4128a
tutyamxx <tutygoo@gmail.com>
2020-02-23 21:47:10 +0000

    Update quote.js

--- commands/fun/quote.js
+++ commands/fun/quote.js
@@ -1,23 +1,18 @@
 const Discord = require("discord.js");
-const getJSON = require("get-json");
+const axios = require("axios");
 const CustomFunctions = require("../../functions/funcs.js");
 
 module.exports.run = async (bot, message, args) =>
 {
     const user = message.author;
-
+    
     await message.channel.startTyping();
 
-    await getJSON("https://quote-garden.herokuapp.com/quotes/random", async (error, response) =>
+    await axios.get("https://quote-garden.herokuapp.com/quotes/random").then(function (response)
     {
-        if(error)
-        {
-            return await message.channel.send(":no_entry: Sorry, I can't seem to find a random quote at the moment...  :crying_cat_face:  :no_entry:").then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
-        }
-
         // --| Remove "" from start and end of string
-        let RandomQuoteText = await response.quoteText.trim();
-        let RandomQuoteAuthor = await response.quoteAuthor;
+        const RandomQuoteText = await response.data.quoteText.trim();
+        const RandomQuoteAuthor = await response.data.quoteAuthor;
 
         const DiscordRichEmbed = new Discord.RichEmbed()
         .setAuthor("Cookie Monsta | Random Quote", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
@@ -26,7 +21,11 @@ module.exports.run = async (bot, message, args) =>
         .setThumbnail("https://i.imgur.com/BdjxI8e.png")
         .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
 
-        await message.channel.send({ embed: DiscordRichEmbed }).then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+        await message.channel.send({ embed: DiscordRichEmbed }).then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
+
+    }).catch(async () =>
+    {
+        return await message.channel.send(":no_entry: Sorry, I can't seem to find a random quote at the moment...  :crying_cat_face:  :no_entry:").then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
     });
 };
 
