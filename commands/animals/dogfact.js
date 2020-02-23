f02fe18d6ae99a36a8571a3cfa0e9341d89b1364
tutyamxx <tutygoo@gmail.com>
2020-02-23 19:38:33 +0000

    Update dogfact.js

--- commands/animals/dogfact.js
+++ commands/animals/dogfact.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) =>
     await axios.get("https://some-random-api.ml/dogfact").then(async (response) =>
     {
         // --| Remove "" from start and end of string
-        const DogFactToString = JSON.stringify(await response.fact).replace(/"/g, "").replace(/\\/g, "``");
+        const DogFactToString = JSON.stringify(await response.data.fact).replace(/"/g, "").replace(/\\/g, "``");
 
         const DiscordRichEmbed = new Discord.RichEmbed()
         .setAuthor("Cookie Monsta | Dog Facts", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
@@ -20,7 +20,7 @@ module.exports.run = async (bot, message, args) =>
         .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
 
         await message.channel.send({ embed: DiscordRichEmbed }).then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
-        
+
     }).catch(async () =>
     {
         return await message.channel.send(":no_entry: Did you know? Dogs can bite internet cables and kill the API connection? :cry:  :no_entry:").then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
