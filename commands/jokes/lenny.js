2397ee7efbeb97d70f099917ed45173eca405873
tutyamxx <tutygoo@gmail.com>
2020-02-23 21:21:37 +0000

    Update lenny.js

--- commands/jokes/lenny.js
+++ commands/jokes/lenny.js
@@ -1,21 +1,17 @@
 const Discord = require("discord.js");
-const getJSON = require("get-json");
+const axios = require("axios");
 
 module.exports.run = async (bot, message, args) =>
 {
     const user = message.author;
-
     const LennyMessage = await message.channel.send("Fetching some lenny's ( ͜。 ͡ʖ ͜。) ...");
 
-    await getJSON("http://lenny.today/api/v1/random", async (error, response) =>
-    {
-        if(error)
-        {
-            return await message.channel.send(":no_entry: Sorry, it seems our lenny's are sad (ಥ ͜ʖಥ). Try later. :no_entry:");
-        }
+    await message.channel.startTyping();
 
+    await axios.get("http://lenny.today/api/v1/random").then(async (response) =>
+    {
         // --| Remove "" from start and end of string
-        let LennyFace = JSON.stringify(await response[0].face).replace(/"/g, '');
+        const LennyFace = JSON.stringify(await response.data[0].face).replace(/"/g, "");
 
         const DiscordRichEmbed = new Discord.RichEmbed()
         .setAuthor("Cookie Monsta | Your random Lenny", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
@@ -24,7 +20,11 @@ module.exports.run = async (bot, message, args) =>
         .setThumbnail("https://i.imgur.com/TxUxdfi.png")
         .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
 
-        await LennyMessage.edit({ embed: DiscordRichEmbed });
+        await LennyMessage.edit({ embed: DiscordRichEmbed }).then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
+
+    }).catch(async () =>
+    {
+        return await message.channel.send(":no_entry: Sorry, it seems our lenny's are sad (ಥ ͜ʖಥ). Try later. :no_entry:").then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
     });
 };
 
