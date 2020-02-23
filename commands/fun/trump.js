32404d3869eb3845b7dbe7a6ebe29f89d565c74b
tutyamxx <tutygoo@gmail.com>
2020-02-23 21:34:21 +0000

    Update trump.js

--- commands/fun/trump.js
+++ commands/fun/trump.js
@@ -1,19 +1,18 @@
-const getJSON = require("get-json");
+const axios = require("axios");
 
 module.exports.run = async (bot, message, args) =>
 {
     await message.channel.startTyping();
 
-    await getJSON("https://api.tronalddump.io/random/quote", async (error, response) =>
+    await axios.get("https://api.tronalddump.io/random/quote").then(async (response) =>
     {
-        if(error)
-        {
-            return await message.channel.send(":no_entry: Sorry, but somehow I can't fetch any Trump shizz at the moment... :no_entry:").then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
-        }
-
-        let GetTrumpTweets = JSON.stringify(await response._embedded.source[0].url).replace(/"/g, '');
-
-        await message.channel.send(":joy::ok_hand: **Some random dumb Trump tweets** :joy::ok_hand:\n\n" + GetTrumpTweets).then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+        const GetTrumpTweets = JSON.stringify(await response.data._embedded.source[0].url).replace(/"/g, "");
+        
+        await message.channel.send(":joy::ok_hand: **Some random dumb Trump tweets** :joy::ok_hand:\n\n" + GetTrumpTweets).then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
+    
+    }).catch(async () =>
+    {
+        return await message.channel.send(":no_entry: Sorry, but somehow I can't fetch any Trump shizz at the moment... :no_entry:").then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
     });
 };
 
