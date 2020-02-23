7ec1f3b93958706c28e7557c8dcd4c70f22b6872
tutyamxx <tutygoo@gmail.com>
2020-02-23 19:46:18 +0000

    Update koalafact.js

--- commands/animals/koalafact.js
+++ commands/animals/koalafact.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const getJSON = require("get-json");
+const axios = require("axios");
 
 module.exports.run = async (bot, message, args) =>
 {
@@ -7,15 +7,10 @@ module.exports.run = async (bot, message, args) =>
 
     await message.channel.startTyping();
 
-    await getJSON("https://some-random-api.ml/facts/koala", async (error, response) =>
+    await axios.get("https://some-random-api.ml/facts/koala").then(async (response) =>
     {
-        if(error)
-        {
-            return await message.channel.send(":no_entry: Did you know? Koalas are so protected that, they might have disappeared? :cry:  :no_entry:").then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
-        }
-
         // --| Remove "" from start and end of string
-        let KoalaFactToString = JSON.stringify(await response.fact).replace(/"/g, '').replace(/\\/g, "``");
+        const KoalaFactToString = JSON.stringify(await response.data.fact).replace(/"/g, "").replace(/\\/g, "``");
 
         const DiscordRichEmbed = new Discord.RichEmbed()
         .setAuthor("Cookie Monsta | Koala Facts", (bot.user.avatarURL === null) ? bot.user.defaultAvatarURL : bot.user.avatarURL)
@@ -24,7 +19,11 @@ module.exports.run = async (bot, message, args) =>
         .setThumbnail("https://i.imgur.com/Ch7vTxz.png")
         .setFooter("Requested by: @" + user.username, (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL)
 
-        await message.channel.send({ embed: DiscordRichEmbed }).then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+        await message.channel.send({ embed: DiscordRichEmbed }).then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
+
+    }).catch(async () =>
+    {
+        return await message.channel.send(":no_entry: Did you know? Koalas are so protected that, they might have disappeared? :cry:  :no_entry:").then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
     });
 };
 
