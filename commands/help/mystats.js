07043d6ed0549f6a6a0f3eec6e2b52e3485d5798
tutyamxx <tutygoo@gmail.com>
2020-02-24 00:03:25 +0000

    Update mystats.js

--- commands/help/mystats.js
+++ commands/help/mystats.js
@@ -11,15 +11,15 @@ module.exports.run = async (bot, message, args) =>
     const user = message.author;
     const GetGuildID = message.guild.id;
 
-    let GetUserAvatar = (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL;
-    let GetUserName = user.username.replace(/'/g, "`").trim();
+    const GetUserAvatar = (user.avatarURL === null) ? user.defaultAvatarURL : user.avatarURL;
+    const GetUserName = user.username.replace(/'/g, "`").trim();
 
     const szPrefix = await DatabaseImport.CookieMonsta_GetGuildPrefix(message.guild.id);
 
     const GetUserBannerImage = await DatabaseImport.CookieMonsta_GetUserProfileBanner(GetGuildID, user.id);
     const GetBannerInfo = await DatabaseImport.CookieMonsta_GetBannerFromDatabase(GetUserBannerImage);
 
-    message.channel.startTyping();
+    await message.channel.startTyping();
 
     let i1 = await Jimp.read(GetUserAvatar);
     let i2 = await Jimp.read("./BOTImages/UserStats/" + GetBannerInfo.png_file.toString());
@@ -34,9 +34,9 @@ module.exports.run = async (bot, message, args) =>
                 console.log("\x1b[31m*\x1b[0m Whoops! There is your error: \x1b[31m" + err + "\x1b[0m");
             }
 
-            let szUserStats = "Level: " + await DatabaseImport.CookieMonsta_GetUserLevel(GetGuildID, user.id) + "  |  XP: " + await DatabaseImport.CookieMonsta_GetUserPoints(GetGuildID, user.id);
-            let iCookiesAmount = "Cookies: " + await DatabaseImport.CookieMonsta_GetUserCookies(GetGuildID, user.id);
-            let CookiesAmountFormatted = (iCookiesAmount.length > 21) ? iCookiesAmount.slice(0, iCookiesAmount.length - 3) + "..." : iCookiesAmount;
+            const szUserStats = "Level: " + await DatabaseImport.CookieMonsta_GetUserLevel(GetGuildID, user.id) + "  |  XP: " + await DatabaseImport.CookieMonsta_GetUserPoints(GetGuildID, user.id);
+            const iCookiesAmount = "Cookies: " + await DatabaseImport.CookieMonsta_GetUserCookies(GetGuildID, user.id);
+            const CookiesAmountFormatted = (iCookiesAmount.length > 21) ? iCookiesAmount.slice(0, iCookiesAmount.length - 3) + "..." : iCookiesAmount;
 
             await gm(buffer)
             .fill("rgba(0, 0, 0, 0.5)")
@@ -65,12 +65,12 @@ module.exports.run = async (bot, message, args) =>
 
                 if(ChanceToShowTips === 5)
                 {
-                    await message.channel.send("<:cookiemonsta:634866060465537034> **|** Remember, you can change your profile card banner at any time using ``" + szPrefix + "setbanner`` command :thumbsup:\n<:cookiemonsta:634866060465537034> **|** **Server stats :bar_chart: for:** ***" + GetUserName + "***", new Discord.Attachment(buffer2, szStatsFileName)).then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+                    await message.channel.send("<:cookiemonsta:634866060465537034> **|** Remember, you can change your profile card banner at any time using ``" + szPrefix + "setbanner`` command :thumbsup:\n<:cookiemonsta:634866060465537034> **|** **Server stats :bar_chart: for:** ***" + GetUserName + "***", new Discord.Attachment(buffer2, szStatsFileName)).then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
                 }
 
                 else
                 {
-                    await message.channel.send("<:cookiemonsta:634866060465537034> **|** **Server stats :bar_chart: for:** ***" + GetUserName + "***", new Discord.Attachment(buffer2, szStatsFileName)).then(() => message.channel.stopTyping(true)).catch(err => message.channel.stopTyping(true));
+                    await message.channel.send("<:cookiemonsta:634866060465537034> **|** **Server stats :bar_chart: for:** ***" + GetUserName + "***", new Discord.Attachment(buffer2, szStatsFileName)).then(async () => await message.channel.stopTyping(true)).catch(async () => await message.channel.stopTyping(true));
                 }
             });
         });
