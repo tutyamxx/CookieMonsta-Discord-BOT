b4fa49b3fa585273021228e267a237e30554599f
tutyamxx <tutygoo@gmail.com>
2020-03-15 15:30:23 +0000

    Update ascii.js

--- commands/fun/ascii.js
+++ commands/fun/ascii.js
@@ -7,7 +7,12 @@ module.exports.run = async (bot, message, szArgs) =>
 {
     if(CustomFunctions.isEmpty(szArgs[0]))
     {
-        return message.reply(" :no_entry: this parameter can't be empty you scrub :facepalm: ! Make sure you add some text to convert.  :no_entry:");
+        return message.reply(" :no_entry: this parameter can't be empty you scrub :facepalm: ! Make sure you add some text to convert to ASCII art :art:  :no_entry:");
+    }
+
+    if(message.mentions.members.first())
+    {
+        return message.reply(" :no_entry: please don't mention people, just type the text you want to convert into ASCII art :art: ! :no_entry:");
     }
 
     let ArgumentText = szArgs.join(" ");
