577a637cc49fa30ce8e45cbb1fa450fa076e74ab
tutyamxx <tutygoo@gmail.com>
2020-03-15 16:26:15 +0000

    Fixed some undefined errors in guildDelete

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -2,13 +2,16 @@ const Discord = require("discord.js");
 
 module.exports = (bot, guild) =>
 {
-    const DiscordRichEmbed = new Discord.MessageEmbed()
-    .setAuthor("Cookie Monsta | Left the guild: (" + guild.name + ")", bot.user.displayAvatarURL())
-    .setColor(14423100)
-    .setThumbnail(guild.iconURL())
-    .setDescription(":speaking_head::loudspeaker: I have left the guild: **(" + guild.name + ")**")
-    .setFooter("Now in #" + bot.guilds.cache.size + " guilds!")
-    .setTimestamp()
+    if(guild.available && guild !== undefined)
+    {
+        const DiscordRichEmbed = new Discord.MessageEmbed()
+        .setAuthor("Cookie Monsta | Left the guild: (" + guild.name + ")", bot.user.displayAvatarURL())
+        .setColor(14423100)
+        .setThumbnail((guild.iconURL() === null) ? guild.owner.user.displayAvatarURL() : guild.iconURL())
+        .setDescription(":speaking_head::loudspeaker: I have left the guild: **(" + guild.name + ")**")
+        .setFooter("Now in #" + bot.guilds.cache.size + " guilds!")
+        .setTimestamp()
 
-    bot.channels.cache.get("634842132808597505").send({ embed: DiscordRichEmbed });
+        bot.channels.cache.get("634842132808597505").send({ embed: DiscordRichEmbed });
+    }
 };
