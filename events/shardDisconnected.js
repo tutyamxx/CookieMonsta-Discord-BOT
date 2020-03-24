d430c58ab2dc08c2d0b0dd1a97cc7c94b9a88e8b
tutyamxx <tutygoo@gmail.com>
2020-03-24 12:58:18 +0000

    Hopefully this works as intended, still playing with v12

--- events/shardDisconnected.js
+++ events/shardDisconnected.js
@@ -1,4 +1,4 @@
-module.exports = (event, shardID) =>
+module.exports = (bot, event, shardID) =>
 {
     console.log(`\x1b[31m*\x1b[0m Disconnected with code \x1b[31m${event.code}\x1b[0m on shard with ID #\x1b[31m${shardID}\x1b[0m !`);
     process.exit(0);
--- events/shardReconnecting.js
+++ events/shardReconnecting.js
@@ -1,4 +1,4 @@
-module.exports = (id) =>
+module.exports = (bot, shardID) =>
 {
-    console.log(`\x1b[31m*\x1b[0m Shard with ID #\x1b[31m${id}\x1b[0m successfully reconnected!`);
+    console.log(`\x1b[31m*\x1b[0m Shard with ID #\x1b[31m${shardID}\x1b[0m successfully reconnected!`);
 };
